5520b979cabbc3a553e7468f2b91d780fd46194f
Purushottam Kumar <purushottam21028@gmail.com>
2026-02-14 22:50:20 +0530

    Refactor server to use environment variable for port

--- backend/server.js
+++ backend/server.js
@@ -116,6 +116,8 @@ Rules:
     }
 });
 
-app.listen(5000, () => {
-    console.log("Backend running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
